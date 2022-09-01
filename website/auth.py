b9b462d180c8f4c54a8e3cf211f4be63f76cd40d
Matho Avito <matavital13@gmail.com>
2022-09-01 21:10:24 +0300

    a lot of changed in the app

--- website/auth.py
+++ website/auth.py
@@ -1,18 +1,37 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return "<p>Login</p>"
+    data = request.form
+    print(data)
+    return render_template("login.html", user="Matan")
 
 
 @auth.route('/logout')
 def logout():
-    return "<p>logout</p>"
+    return render_template("logout.html", user="Matan")
 
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
-    return "<p>Sign-up</p>"
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Email must be grater than 4 characters.', category='error')
+        elif len(firstName) < 2:
+            flash('First name must be grater than 1 characters.', category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match.', category='error')
+        elif len(password1) < 7:
+            flash('Passwords must be at least 7 characters.', category='error')
+        else:
+            flash('Account created!', category='success')
+            
+    return render_template("sign_up.html", user="Matan")
