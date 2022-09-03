943a284f35c2e01588b54fe382d2b8a745a90122
Matho Avito <matavital13@gmail.com>
2022-09-03 18:26:18 +0300

    app is working, changed the username of SQLAlchemy

--- website/config.py
+++ website/config.py
@@ -1,9 +1,9 @@
 import os
 
-user = os.environ.get('MYSQL_USER')
+# user = os.environ.get('MYSQL_USER')
 password = os.environ.get('MYSQL_ROOT_PASSWORD')
 host = os.environ.get('MYSQL_HOST')
 database = os.environ.get('MYSQL_DATABASE')
-port = os.environ.get('MYSQL_PORT')
+port = 3306
 
-DATABASE_CONNECTION_URI = f'mysql+pymysql://{user}:{password}@{host}:{port}/{database}'
+DATABASE_CONNECTION_URI = f'mysql+pymysql://root:{password}@{host}:{port}/{database}'
