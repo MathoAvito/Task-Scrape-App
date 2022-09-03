ca6e2999ddff5d74dab67fdc110e92b4db9be5b1
Matho Avito <matavital13@gmail.com>
2022-09-03 15:08:32 +0300

    minor changes, app broke again

--- website/config.py
+++ website/config.py
@@ -4,6 +4,6 @@ user = os.environ.get('MYSQL_USER')
 password = os.environ.get('MYSQL_ROOT_PASSWORD')
 host = os.environ.get('MYSQL_HOST')
 database = os.environ.get('MYSQL_DATABASE')
-port = 3306
+port = os.environ.get('MYSQL_PORT')
 
 DATABASE_CONNECTION_URI = f'mysql+pymysql://{user}:{password}@{host}:{port}/{database}'
